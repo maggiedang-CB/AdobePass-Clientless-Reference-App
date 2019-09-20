26f20d45cf8a235f9060b88d380ec22f59221463
Maggie Dang <maggie.dang@clearbridgemobile.com>
2019-09-20 12:40:12 -0400

    Added auto generated sample data for adobe auth form

--- app/src/main/java/com/example/android/adobepassclientlessrefapp/AdobeAuthActivity.java
+++ app/src/main/java/com/example/android/adobepassclientlessrefapp/AdobeAuthActivity.java
@@ -1,13 +1,79 @@
 package com.example.android.adobepassclientlessrefapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.android.adobepassclientlessrefapp.ui.AbstractActivity;
+import com.example.android.adobepassclientlessrefapp.adobeauth.GenerateSampleData;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AdobeAuthActivity extends AbstractActivity {
+
+    private String TAG = "AdobeAuthActivity";
+
+    @BindView(R.id.btn_adobe_auth_back)
+    Button backButton;
+    @BindView(R.id.btn_adobe_auth_ok)
+    Button okButton;
+    @BindView(R.id.btn_adobe_auth_generate)
+    Button generateButton;
+    // TODO: Get browse button to work >>> Should lead to url opened in the web?
+    @BindView(R.id.btn_adobe_auth_browse)
+    Button browseButton;
+
+    @BindView(R.id.baseurl)
+    EditText baseUrl;
+    @BindView(R.id.reggieCodePath)
+    EditText reggieCodePath;
+    @BindView(R.id.checkAuthenticationPath)
+    EditText checkAuthenticationPath;
+    @BindView(R.id.authnTokenTimeoutSeconds)
+    EditText authnTokenTimeoutSeconds;
+    @BindView(R.id.tempPassProvider)
+    EditText tempPassProvider;
+    @BindView(R.id.getUserMetadataPath)
+    EditText getUserMetadataPath;
+    @BindView(R.id.authnTokenPath)
+    EditText authnTokenPath;
+    @BindView(R.id.temppassurl)
+    EditText tempPassUrl;
+    @BindView(R.id.authzTokenPath)
+    EditText authzTokenPath;
+    @BindView(R.id.authorizePath)
+    EditText authorizePath;
+    @BindView(R.id.logoutPath)
+    EditText logoutPath;
+    @BindView(R.id.mvpdListPath)
+    EditText mvpdListPath;
+    @BindView(R.id.redirecturl)
+    EditText redirectUrl;
+    @BindView(R.id.tokenizationurl)
+    EditText tokenizationUrl;
+    @BindView(R.id.logosurl)
+    EditText logosUrl;
+    @BindView(R.id.externalBrowserDomains)
+    EditText externalBrowserDomains;
+    @BindView(R.id.nbcTokenurl)
+    EditText nbcTokenUrl;
+
+    //TODO: figure out tempPassSelection when working with temp pass
+
+    // List of key values
+    private ArrayList<String> listOfValues;
+    // list of edit text form
+    private ArrayList<EditText> listOfEditText;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +83,123 @@ public class AdobeAuthActivity extends AbstractActivity {
         // Hide keyboard on launch
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
+        // List of key values
+        this.listOfValues = getFormNamesArray();
+        // list of edit text form
+        this.listOfEditText = getFormArray();
+
+        // setup listeners
+        backButton.setOnClickListener(backButtonListener());
+        generateButton.setOnClickListener(generateListener);
     }
+
+    /**
+     * Go back to main Activity
+     * @return
+     */
+    private Button.OnClickListener backButtonListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: prompt user if they really want to go back if theres data un saved in form
+                Intent intent = new Intent(AdobeAuthActivity.this, MainActivity.class);
+                setResult(RESULT_CANCELED, intent);
+                finish();
+            }
+        };
+    }
+
+    private Button.OnClickListener generateListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.d(TAG,"Clicked generate button");
+            // Uses stored sample data needed for adobe auth
+            JSONObject sampleJson = GenerateSampleData.makeJsonSample();
+            // Add sample data to edit text views
+            generateSampleDataInEditText(sampleJson);
+
+        }
+    };
+
+    /**
+     * Fills out edit text form with the sample data generated from GenerateSampleData.java
+     * @param json
+     */
+    private void generateSampleDataInEditText(JSONObject json) {
+
+        try {
+            int index = 0;
+            for (EditText editText : listOfEditText) {
+                editText.setText(json.getString(listOfValues.get(index)));
+
+                // debug
+                Log.d(TAG, "listOfvalue : " + listOfValues.get(index));
+                Log.d(TAG, "edit text form text: " + listOfEditText.get(index).getText());
+
+                index++;
+            }
+        } catch (JSONException e){
+            Log.d(TAG, "Error generating text to adobe auth form");
+        }
+    }
+
+    /**
+     * Returns an array list of all the edit text views in the adobe auth setup form
+     * @return
+     */
+    private ArrayList<EditText> getFormArray() {
+        ArrayList<EditText> arrayForm = new ArrayList<>();
+        // Note: Do not change order of views added. They correspond to the order appeared on the form
+        arrayForm.add(baseUrl);
+        arrayForm.add(reggieCodePath);
+        arrayForm.add(checkAuthenticationPath);
+        arrayForm.add(authnTokenTimeoutSeconds);
+        arrayForm.add(tempPassProvider);
+        arrayForm.add(getUserMetadataPath);
+        arrayForm.add(authnTokenPath);
+        arrayForm.add(tempPassUrl);
+        arrayForm.add(authzTokenPath);
+        arrayForm.add(authorizePath);
+        arrayForm.add(logoutPath);
+        arrayForm.add(mvpdListPath);
+        arrayForm.add(redirectUrl);
+        arrayForm.add(tokenizationUrl);
+        arrayForm.add(logosUrl);
+        arrayForm.add(externalBrowserDomains);
+        arrayForm.add(nbcTokenUrl);
+
+        return arrayForm;
+    }
+
+    /**
+     * Returns a list of strings containing the name of each form value.
+     * i.e) "tempPassUrl"
+     * @return
+     */
+    private ArrayList<String> getFormNamesArray() {
+        ArrayList<String> formNames = new ArrayList<>();
+
+        formNames.add(this.getString(R.string.auth1_baseUrl));
+        formNames.add(this.getString(R.string.auth2_reggieCodePath));
+        formNames.add(this.getString(R.string.auth3_checkAuthenticationPath));
+        formNames.add(this.getString(R.string.auth4_authnTokenTimeoutSeconds));
+        formNames.add(this.getString(R.string.auth5_tempPassProvider));
+        formNames.add(this.getString(R.string.auth6_getUserMetadataPath));
+        formNames.add(this.getString(R.string.auth7_authnTokenPath));
+        formNames.add(this.getString(R.string.auth8_tempPassUrl));
+        formNames.add(this.getString(R.string.auth9_authzTokenPath));
+        formNames.add(this.getString(R.string.auth10_authorizePath));
+        formNames.add(this.getString(R.string.auth11_logoutPath));
+        formNames.add(this.getString(R.string.auth12_mvpdListPath));
+        formNames.add(this.getString(R.string.auth13_redirectUrl));
+        formNames.add(this.getString(R.string.auth14_tokenizationUrl));
+        formNames.add(this.getString(R.string.auth15_logosUrl));
+        formNames.add(this.getString(R.string.auth16_externalBrowserDomains));
+        formNames.add(this.getString(R.string.auth17_nbcTokenUrl));
+        formNames.add(this.getString(R.string.auth18_tempPassSelection));
+
+        return formNames;
+    }
+
 
 }
---
+++ app/src/main/java/com/example/android/adobepassclientlessrefapp/adobeauth/GenerateSampleData.java
@@ -0,0 +1,83 @@
+package com.example.android.adobepassclientlessrefapp.adobeauth;
+
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Uses sample data to generate and auto fill adobe auth form
+ */
+public class GenerateSampleData {
+
+    private static String TAG = "GenerateSampleData";
+
+    public static String sampleJsonString = "{\n" +
+            "\t\t\"baseUrl\": \"https://api.auth.adobe.com\",\n" +
+            "\t\t\"checkAuthenticationPath\": \"/api/v1/checkauthn?deviceId={DEVICE_ID}\",\n" +
+            "\t\t\"reggieCodePath\": \"/reggie/v1/{REQUESTOR_ID}/regcode\",\n" +
+            "\t\t\"registrationUrl\": \"https://dev.ott.nbcsports.com/\",\n" +
+            "\t\t\"authenticatePath\": \"/api/v1/authenticate?reg_code={REGGIE_CODE}&requestor_id={REQUESTOR_ID}&domain_name=adobe.com&noflash=true&no_iframe=true&mso_id={MSO_ID}&redirect_url={REDIRECT_URL}\",\n" +
+            "\t\t\"authnTokenPath\": \"/api/v1/tokens/authn\",\n" +
+            "\t\t\"authnTokenCheckInterval\": 10,\n" +
+            "\t\t\"authnTokenTimeoutSeconds\": 60,\n" +
+            "\t\t\"authorizePath\": \"/api/v1/authorize?deviceId={DEVICE_ID}&requestor={REQUESTOR_ID}&resource={RESOURCE}\",\n" +
+            "\t\t\"authzTokenPath\": \"/api/v1/tokens/authz?deviceId={DEVICE_ID}&requestor={REQUESTOR_ID}&resource={RESOURCE}\",\n" +
+            "\t\t\"mediaTokenPath\": \"/api/v1/tokens/media?deviceId={DEVICE_ID}&requestor={REQUESTOR_ID}&resource={RESOURCE}\",\n" +
+            "\t\t\"tokenizationUrl\": \"http://stream.nbcsports.com/data/mobile/passnbc.xml\",\n" +
+            "\t\t\"redirectUrl\": \"https://smart.link/5be5f39d4b556\",\n" +
+            "\t\t\"logoutPath\": \"/api/v1/logout\",\n" +
+            "\t\t\"tempPassUrl\": \"/api/v1/authenticate/freepreview\",\n" +
+            "\t\t\"tempPassRequestor\": \"nbcsports\",\n" +
+            "\t\t\"tempPassProvider\": \"TempPass_10min\",\n" +
+            "\t\t\"preauthorizePath\": \"/api/v1/preauthorize\",\n" +
+            "\t\t\"preauthorizeBody\": \"deviceType=android&deviceId={DEVICE_ID}&appId=RSN%20App&ttl=300&requestor={REQUESTOR_ID}&resource=CSNChicago,CSNPhiladelphia,CSNCalifornia,CSNNorthwest,CSNNewEngland,CSNMidAtlantic,CSNBayArea\",\n" +
+            "\t\t\"getUserMetadataPath\": \"/api/v1/tokens/usermetadata?deviceType=android&deviceId={DEVICE_ID}&requestor={REQUESTOR_ID}\",\n" +
+            "\t\t\"logosUrl\": \"http://stream.nbcsports.com/data/mobile/mvpd_logos_doc.json\",\n" +
+            "\t\t\"mvpdListPath\": \"/api/v1/config\",\n" +
+            "\t\t\"externalBrowserDomains\": [\n" +
+            "\t\t\t\"YouTubeTV\",\n" +
+            "\t\t\t\"GoogleFiber\",\n" +
+            "\t\t\t\"googlefiber_auth-gateway_net\"],\n" +
+            "\t\t\"nbcTokenUrl\": \"https://token.playmakerservices.com/cdn\",\n" +
+            "\t\t\"tempPassSelection\": {\n" +
+            "\t\t\t\"requestorId\": \"nbcsports\",\n" +
+            "\t\t\t\"deviceType\": \"Android\",\n" +
+            "\t\t\t\"passes\": [{\n" +
+            "\t\t\t\t\"requestorID\": \"NBCOlympics\",\n" +
+            "\t\t\t\t\"shortTempPassId\": \"TempPass-ShortTTL\",\n" +
+            "\t\t\t\t\"longTempPassId\": \"TempPass-NBCSPORTS-LongTTL\"\n" +
+            "\t\t\t}, {\n" +
+            "\t\t\t\t\"requestorID\": \"nbcentertainment\",\n" +
+            "\t\t\t\t\"shortTempPassId\": \"TempPass-ShortTTL\",\n" +
+            "\t\t\t\t\"longTempPassId\": \"TempPass-NBCSPORTS-LongTTL\"\n" +
+            "\t\t\t}, {\n" +
+            "\t\t\t\t\"requestorID\": \"telemundo\",\n" +
+            "\t\t\t\t\"shortTempPassId\": \"TempPass-ShortTTL\",\n" +
+            "\t\t\t\t\"longTempPassId\": \"TempPass-NBCSPORTS-LongTTL\"\n" +
+            "\t\t\t}, {\n" +
+            "\t\t\t\t\"requestorID\": \"nbcsports\",\n" +
+            "\t\t\t\t\"shortTempPassId\": \"TempPass-Sports-10min\",\n" +
+            "\t\t\t\t\"longTempPassId\": \"\"\n" +
+            "\t\t\t}, {\n" +
+            "\t\t\t\t\"requestorID\": \"golf\",\n" +
+            "\t\t\t\t\"shortTempPassId\": \"TempPass-Golf-10min\",\n" +
+            "\t\t\t\t\"longTempPassId\": \"\"\n" +
+            "\t\t\t}]\n" +
+            "\t\t}\n" +
+            "\t}";
+
+
+    public static JSONObject makeJsonSample() {
+        try {
+            JSONObject json = new JSONObject(sampleJsonString);
+            Log.d(TAG, json.toString());
+            return json;
+        } catch (JSONException e) {
+            Log.d(TAG, "ERROR when converting sample json string data to Json object");
+        }
+        return null;
+    }
+
+}
