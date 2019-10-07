92962591e1740884e213079c1d398eacef2821a7
Maggie Dang <maggie.dang@clearbridgemobile.com>
2019-10-07 09:32:51 -0400

    LEAPP-4874 Added "Authorizing Video..." toast message after clicking PLAY MEDIA.

--- app/src/main/java/com/example/android/adobepassclientlessrefapp/MainActivity.java
+++ app/src/main/java/com/example/android/adobepassclientlessrefapp/MainActivity.java
@@ -344,6 +344,7 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 // Authorize and launch video player
                 progressSpinner.setVisibility(View.VISIBLE);
+                showToast("Authorizing Video...");
                 authorize(true);
                 addToLogcat("ExoPlayer Launched");
             }
